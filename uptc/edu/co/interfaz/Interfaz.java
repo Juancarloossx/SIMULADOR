e30d5fbc68feb9de27264161a98415b1477f4b4c
Juan Linux <Juancalinux@hotmail.com>
2024-02-29 15:21:52 -0500

    Se cresaron los paquetes correspondientes junto con unas clases basicas

---
+++ uptc/edu/co/interfaz/Interfaz.java
@@ -0,0 +1,19 @@
+package uptc.edu.co.interfaz;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+public class Interfaz  extends JFrame{
+    public Interfaz(){
+        super("Primer ventana");
+
+        // Establecer el tamaño de la ventana
+        setSize(400, 300);
+
+        // Establecer la operación de cierre
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+
+    }
+
+
---
+++ uptc/edu/co/model/Main.java
@@ -0,0 +1,11 @@
+package uptc.edu.co.model;
+
+import uptc.edu.co.interfaz.Interfaz;
+
+public class Main {
+    public static void main(String[] args) {
+        Interfaz interfaz = new Interfaz();
+
+        interfaz.setVisible(true);
+    }
+}
